e1d30be2450297dd8b711fff8591aaa19d84e6a0
Tristan <kraig.tri125@gmail.com>
2016-06-03 00:00:48 -0400

    POST and response

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/tri125/sudoku"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,7 +15,8 @@ func main() {
 	portString := strconv.Itoa(port)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", HomeHandler).Methods("GET")
+	r.HandleFunc("/", SolveHandler).Methods("POST")
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 	http.Handle("/", r)
 
@@ -38,4 +41,48 @@ func HomeHandler(w http.ResponseWriter, req *http.Request) {
 		log.Print("template/home error:", err)
 	}
 	t.ExecuteTemplate(w, "home.html", nil)
+}
+
+func SolveHandler(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	var gridPost []string
+
+	if err != nil {
+		log.Print("SolveHandler ParseForm error:", err)
+	}
+
+	for key, values := range req.PostForm {
+		if key == "value[]" {
+			gridPost = values
+		} else {
+			continue
+		}
+
+	}
+
+	if gridPost != nil {
+		var grid sudoku.Grid
+		var count int = 0
+		for x := 0; x < len(grid); x++ {
+			for y := 0; y < len(grid[x]); y++ {
+				gridValue, err := strconv.Atoi(gridPost[count])
+				if err != nil {
+					log.Print("Post grid atoi error:", err)
+				}
+				grid[x][y] = gridValue
+				count++
+			}
+		}
+		/*grid, err := sudoku.SolveGrid(grid)
+		if err != nil {
+			log.Print("Sudoku Grid Solver error:", err)
+		}*/
+		jsonResponse, err := json.Marshal(grid)
+
+		if err != nil {
+			log.Print("Error:", err)
+		}
+		w.Write(jsonResponse)
+	}
+
 }
